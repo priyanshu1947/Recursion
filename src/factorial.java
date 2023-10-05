28ad006bde64a1161c831bab715d1d2e3f7524c7
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-10-05 09:05:55 +0530

    factorial of n number using recursion.

---
+++ src/factorial.java
@@ -0,0 +1,21 @@
+public class factorial
+{
+    public static void main(String[] args)
+    {
+        int ans = fac(5);
+        System.out.println(ans);
+
+    }
+    static int fac(int n)
+    {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n*fac(n-1);
+
+
+    }
+
+
+}
