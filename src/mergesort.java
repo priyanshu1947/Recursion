d7d4bbec1f616f7ef406044ec5f49909f0a92010
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-10-05 20:00:27 +0530

    merge sort other approach  using recursion.

--- src/mergesort.java
+++ src/mergesort.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 public class mergesort {
     public static void main(String[] args) {
 int [] arr = {5,4,3,2,1};
-arr = mergesort(arr);
+//arr = mergesort(arr);
+        //System.out.println(Arrays.toString(arr));
+        mergesortinplace(arr,0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
-
     static int[] mergesort(int[] arr) {
         if (arr.length == 1)
             return arr;
@@ -54,6 +55,59 @@ arr = mergesort(arr);
             k++;
         }
         return mix;
+
+
+    }
+
+    static void mergesortinplace(int[] arr,int start, int end) {
+        if (end-start == 1)
+            return;
+
+        int mid = (start+end) / 2;
+
+        mergesortinplace(arr, start, mid);
+        mergesortinplace(arr, mid,end);
+
+        merge(arr,start,mid,end);
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int [] mix = new int [end-start];
+
+        int i =start;
+        int j = mid;
+        int k = 0;
+
+        while(i<mid && j<end)
+        {
+            if(arr[i]<arr[j])
+            {
+                mix[k]= arr[i];
+                i++;
+            }
+            else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // it is possible that one array is not completed
+        while(i<mid)
+        {
+            mix[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<end)
+        {
+            mix[k]=arr[j];
+            j++;
+            k++;
+        }
+        for(int l =0; l<mix.length;l++)
+        {
+            arr[start+l]= mix[l];
+        }
 
 
     }
