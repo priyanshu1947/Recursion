9df005bf23b2c0b250c071ac0e47327b40234d33
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-10-05 09:55:08 +0530

    reverse digit of number using recursion.

---
+++ src/reversedigit.java
@@ -0,0 +1,21 @@
+public class reversedigit
+{
+    static int sum = 0;
+    static void reverse(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10+rem;
+        reverse(n/10);
+    }
+
+    public static void main(String[] args) {
+     reverse(12345);
+        System.out.println(sum);
+
+    }
+
+}
