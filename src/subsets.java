7c1a3095c7fa7e8acfc54f18b055f4a1b6e02df6
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-10-08 00:02:42 +0530

    return arrayList all the subset of  a string using recursion.

--- src/subsets.java
+++ src/subsets.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class subsets
 {
     public static void main(String[] args)
     {
-        subseq("","abc");
+        System.out.println(subseq1("","abc"));
     }
     static void subseq(String p, String up)
     {
@@ -15,5 +17,23 @@ public class subsets
 
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
+    }
+
+    static ArrayList<String > subseq1(String p, String up)
+    {
+        if(up.isEmpty())
+        {
+            ArrayList<String > list = new ArrayList<>();
+            list.add(p);
+            return list;
+
+        }
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subseq1(p+ch,up.substring(1));
+        ArrayList<String> right = subseq1(p,up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
